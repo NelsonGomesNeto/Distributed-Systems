4e51fa5176ca8ab359459d60f28b9dacbfcf131b
Nelson Gomes Neto <ngn@ic.ufal.br>
2019-08-09 01:16:42 -0300

    Fix B_sync_multicast

--- Multicast/multicast.jl
+++ Multicast/multicast.jl
@@ -1,41 +1,61 @@
 using Distributed
 using Plots
+using StatsPlots
+
+iterations = 100
+addprocs(15)
+
+@everywhere function B_deliver(message)
+  println(message)
+  flush(stdout)
+end
 
 function B_sync_multicast(group, message, B_deliver)
+  for i in group
+    remotecall_fetch(B_deliver, i, message)
+  end
+end
+
+function B_async_multicast(group, message, B_deliver)
   @sync for i in group
     remotecall(B_deliver, i, message)
   end
 end
 
-function B_async_multicast(group, message, B_deliver)
-  @sync @distributed for i in group
-    remotecall(B_deliver, i, message)
-  end
-end
-
-addprocs(2)
-
 group = workers()
 
-syncTimes, asyncTimes = [], []
-for i in 1:50
-  push!(syncTimes, @elapsed B_sync_multicast(group, "Hi hi", println))
-  push!(asyncTimes, @elapsed B_async_multicast(group, "Hi hi", println))
+syncTimes = []
+syncTotalTime = @elapsed for i in 1:iterations
+  println("$i -----------------------------------")
+  flush(stdout)
+  push!(syncTimes, @elapsed B_sync_multicast(group, "Sync hi  -- $i", B_deliver))
 end
 
-println("------------------------------------------")
-println("------------------------------------------")
+asyncTimes = []
+asyncTotalTime = @elapsed for i in 1:iterations
+  println("$i -----------------------------------")
+  flush(stdout)
+  push!(asyncTimes, @elapsed B_async_multicast(group, "Async hi -- $i", B_deliver))
+end
 
-println("syncTimes: $syncTimes")
-println("asyncTimes: $asyncTimes")
-
-println("------------------------------------------")
-println("------------------------------------------")
-
-deleteat!(syncTimes, UnitRange(1, 5))
-deleteat!(asyncTimes, UnitRange(1, 5))
+deleteat!(syncTimes, UnitRange(1, 10))
+deleteat!(asyncTimes, UnitRange(1, 10))
 syncTimes, asyncTimes = Array{Float64}(syncTimes), Array{Float64}(asyncTimes)
-plot(1:45, [syncTimes, asyncTimes])
-# plot(1:50, [syncTimes, asyncTimes])
 
-read()
+f = open("data B_multicast", "w")
+println(f, "Sync total time: $syncTotalTime || Async total time: $asyncTotalTime")
+println(f, "Sync: $syncTimes")
+println(f, "Async: $asyncTimes")
+
+# p = plot(1:length(syncTimes),
+#          [syncTimes, asyncTimes],
+#          title="Sync x Async B_multicast",
+#          label=["Sync", "Async"],
+#          xlabel="Runs",
+#          ylabel="Time (s)")
+p = boxplot(["Sync", "Async"],
+            [syncTimes, asyncTimes],
+            title="Sync x Async B_multicast",
+            legend=false,
+            ylabel="Time (s)")
+savefig(p, "B_multicast")
