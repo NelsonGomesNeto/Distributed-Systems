f81ae53e53c9fb4f476142ca55ea5176a9e98634
Nelson Gomes Neto <ngn@ic.ufal.br>
2019-08-13 01:22:11 -0300

    Finish B_multicast and R_multicast

--- Multicast/multicast.jl
+++ Multicast/multicast.jl
@@ -2,8 +2,10 @@ using Distributed
 using Plots
 using StatsPlots
 
+z = 1.96 # 95% confidence level
+
 iterations = 100
-addprocs(15)
+addprocs(10)
 
 @everywhere function B_deliver(message)
   println(message)
@@ -17,7 +19,7 @@ function B_sync_multicast(group, message, B_deliver)
 end
 
 function B_async_multicast(group, message, B_deliver)
-  @sync for i in group
+  @sync @distributed for i in group
     remotecall(B_deliver, i, message)
   end
 end
@@ -25,25 +27,31 @@ end
 group = workers()
 
 syncTimes = []
-syncTotalTime = @elapsed for i in 1:iterations
+for i in 1:iterations
   println("$i -----------------------------------")
   flush(stdout)
   push!(syncTimes, @elapsed B_sync_multicast(group, "Sync hi  -- $i", B_deliver))
 end
 
 asyncTimes = []
-asyncTotalTime = @elapsed for i in 1:iterations
+for i in 1:iterations
   println("$i -----------------------------------")
   flush(stdout)
   push!(asyncTimes, @elapsed B_async_multicast(group, "Async hi -- $i", B_deliver))
 end
 
-deleteat!(syncTimes, UnitRange(1, 10))
-deleteat!(asyncTimes, UnitRange(1, 10))
 syncTimes, asyncTimes = Array{Float64}(syncTimes), Array{Float64}(asyncTimes)
+sort!(syncTimes), sort!(asyncTimes)
+deleteat!(syncTimes, UnitRange(length(syncTimes) - 10, length(syncTimes)))
+deleteat!(asyncTimes, UnitRange(length(asyncTimes) - 10, length(asyncTimes)))
+syncTotalTime, asyncTotalTime = sum(syncTimes), sum(asyncTimes)
+syncMeanTime, asyncMeanTime = syncTotalTime / length(syncTimes), asyncTotalTime / length(asyncTimes)
+syncStdTime, asyncStdTime = z * syncMeanTime / sqrt(length(syncTimes)), z * asyncMeanTime / sqrt(length(asyncTimes))
 
 f = open("data B_multicast", "w")
 println(f, "Sync total time: $syncTotalTime || Async total time: $asyncTotalTime")
+println(f, "Sync mean time: $syncMeanTime || Async mean time: $asyncMeanTime")
+println(f, "Sync std time: $syncStdTime || Async std time: $asyncStdTime")
 println(f, "Sync: $syncTimes")
 println(f, "Async: $asyncTimes")
 
@@ -53,9 +61,11 @@ println(f, "Async: $asyncTimes")
 #          label=["Sync", "Async"],
 #          xlabel="Runs",
 #          ylabel="Time (s)")
-p = boxplot(["Sync", "Async"],
-            [syncTimes, asyncTimes],
-            title="Sync x Async B_multicast",
-            legend=false,
-            ylabel="Time (s)")
+p = groupedbar(["Sync", "Async"],
+               [syncMeanTime asyncMeanTime],
+               yerr=[syncStdTime asyncStdTime],
+               group=["Sync", "Async"],
+               title="Sync x Async B_multicast",
+               ylabel="Time (seconds)",
+               bar_width=0.7)
 savefig(p, "B_multicast")
